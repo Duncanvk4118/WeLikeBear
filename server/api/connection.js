4fa8d62beb77e0eec7cfbc7ca5e5eccedb779dce
Kevin Pierik <kevinpierik@icloud.com>
2025-02-11 09:00:07 +0100

    chore(api): voegt .env variabelen

--- server/api/connection.js
+++ server/api/connection.js
@@ -1,12 +1,13 @@
 import mysql from 'mysql2/promise';
+
 export default defineEventHandler(async (event) => {
     // Verbinding request maken
     const connection = await mysql.createConnection({
-        host: 'localhost',
-        port: 8889,
-        user: 'root',
-        password: 'root',
-        database: 'BierLijst'
+        host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE
     });
 
     // SQL syntax

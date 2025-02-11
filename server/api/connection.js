6e83a8b648a13fa8b4f6c2186def962b97787137
Duncanvk4118 <duncan.v.keulen@gmail.com>
2025-02-11 12:02:16 +0100

    Database Resolved

--- server/api/connection.js
+++ server/api/connection.js
@@ -3,12 +3,10 @@ import mysql from 'mysql2/promise';
 export default defineEventHandler(async (event) => {
     const method = event.node.req.method;
 
-    // Haal de ID uit de URL voor de PUT-aanroep
-    const { id } = event.context.params;  // Dit haalt de ID op uit de URL.
-
     // Maak de databaseverbinding
     const connection = await mysql.createConnection({
         host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE
--- server/api/connection/[id].js
+++ server/api/connection/[id].js
@@ -15,6 +15,7 @@ export default defineEventHandler(async (event) => {
     // Databaseverbinding opzetten
     const connection = await mysql.createConnection({
         host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE
